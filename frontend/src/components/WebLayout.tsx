8887be747e3cc70a9fe9779ab3fe08281d259f87
Shuyang Li <shuyang.li.95@gmail.com>
2025-06-01 15:29:23 -0700

    Make WebLayout full height

--- frontend/src/components/WebLayout.tsx
+++ frontend/src/components/WebLayout.tsx
@@ -4,9 +4,9 @@ import "./WebLayout.css";
 
 export const WebLayout = () => {
   return (
-    <div className="app-container flex flex-col min-h-screen w-screen items-center bg-gray-100">
+    <div className="app-container flex flex-col h-full w-screen items-center bg-gray-100">
       <TopNav />
-      <main className="trips-main flex-grow w-full flex justify-center py-8 px-4">
+      <main className="trips-main flex-grow w-full h-full flex justify-center py-8 px-4">
         <Outlet />
       </main>
     </div>
